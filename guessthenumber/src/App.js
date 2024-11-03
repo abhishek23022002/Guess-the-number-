c71eb8824613b2cde5f417c22051b1ffabfd668a
Abhishek <sabhishekbabu@gmail.com>
2024-11-03 10:52:15 +0530

    UI path ready

--- guessthenumber/src/App.js
+++ guessthenumber/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState} from "react"
 
 function App() {
+  const [term, setterm]= useState("")
+  
+  function clicking(params) {
+    setterm(params.target.value)
+    console.log("Text",term)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="head">
+        <label htmlFor="term"> Enter the number beteen 1 to 10</label>
+      </div>
+      <input type="text" id="term" name="term " onChange={clicking}/>
     </div>
   );
 }
